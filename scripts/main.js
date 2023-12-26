7abae0ade7c6e83d0d032f05eeece715b0d9df03
Hotchpok <144107666+Hotchpok@users.noreply.github.com>
2023-12-26 17:54:16 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const wrapper = document.querySelector(".wrapper");
+    const question = document.querySelector(".question");
+    const gif = document.querySelector(".gif");
+    const yesBtn = document.querySelector(".yes-btn");
+    const noBtn = document.querySelector(".no-btn");
 
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
+    yesBtn.addEventListener("click", () => {
+        question.innerHTML = "Aaaaa, I like you too";
+        gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+    });
 
-yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaa, I like you too";
-    gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
-});
-
-noBtn.addEventListener("mouseover", () => {
-    moveButtonRandomly(noBtn);
+    noBtn.addEventListener("mouseover", () => {
+        moveButtonRandomly(noBtn);
+    });
 });
 
 function moveButtonRandomly(button) {
