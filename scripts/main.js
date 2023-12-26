316055d35c6736b17609d4257a24c7243e75dd12
Hotchpok <144107666+Hotchpok@users.noreply.github.com>
2023-12-26 17:40:37 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,10 +14,9 @@ noBtn.addEventListener("mouseover", () => {
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
 
-
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
     noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+    noBtn.style.left = randomy + "px";
 });
