ab679b29fdfe9c1e73c31aed4c10f1e16d06c35e
Hotchpok <144107666+Hotchpok@users.noreply.github.com>
2023-12-26 17:48:57 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
@@ -10,13 +11,21 @@ yesBtn.addEventListener("click", () => {
 });
 
 noBtn.addEventListener("mouseover", () => {
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+    moveButtonRandomly(noBtn);
 });
+
+function moveButtonRandomly(button) {
+    const buttonRect = button.getBoundingClientRect();
+    const maxX = window.innerWidth - buttonRect.width;
+    const maxY = window.innerHeight - buttonRect.height;
+
+    const randomX = getRandomNumber(0, maxX);
+    const randomY = getRandomNumber(0, maxY);
+
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
